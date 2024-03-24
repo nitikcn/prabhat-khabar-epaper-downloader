6276f93924fa0f2a406ac3747598c155b9593fc9
nitikcn <143488043+nitikcn@users.noreply.github.com>
2024-03-24 23:05:50 +0530

    Update prabhat_khabar_epaper.py

--- prabhat_khabar_epaper.py
+++ prabhat_khabar_epaper.py
@@ -1,7 +1,5 @@
-#!/bin/python3
-# 24032024, Sun
-# Prabhat Khabar E-Paper downloader
-## Great, it worked like a charm on the first time I ran it. Downloaded all the webpages, just failed to detect that the last page has been downloaded. Love it. Well, I was careful and thinking about the program well from the beginning too.
+## Prabhat Khabar E-Paper downloader
+## 24-03-2024, Monday
 
 import os
 import sys
@@ -14,6 +12,8 @@ import datetime as dt
 
 
 today_isoformat_date = dt.date.isoformat(dt.datetime.now())     # like 2024-03-23
+
+# Check if the argument date is correctly formatted
 if len(sys.argv) > 1:
     if len(sys.argv[1]) == 10:
         date_regex = re.compile('\d{4}-\d{2}-\d{2}')
@@ -22,11 +22,12 @@ if len(sys.argv) > 1:
             today_isoformat_date = sys.argv[1]
         else:
             raise Exception('Could not understand the given date')
-cwd = f'/tmp/my_dir/Prabhat Khabar E-Papers/{today_isoformat_date}'
+            
+cwd = f'./Prabhat Khabar E-Papers/{today_isoformat_date}'
 os.makedirs(cwd, exist_ok=True)
 os.chdir(cwd)
 
-# Full url is like https://epaper.prabhatkhabar.com/patna/patna-city/2024-03-23/1, num after last solidus is page num
+# Full url is like https://epaper.prabhatkhabar.com/patna/patna-city/2024-03-23/1, the number after the last slash is page num
 epaper_part_url = 'https://epaper.prabhatkhabar.com/patna/patna-city/' + today_isoformat_date
 base_url_for_pic = 'https://epaper.prabhatkhabar.com/'
 
@@ -34,10 +35,10 @@ base_url_for_pic = 'https://epaper.prabhatkhabar.com/'
 for i in range(1, 50):  # chose max page num to be 50 randomly
     page_url = f'{epaper_part_url}/{i}'
 
+    # Download page url at first
     try_counter = 0
     while try_counter < 6:
         try:
-        ### Think about how to detect that last page has been loaded
             try_counter += 1;
             page_html = requests.get(page_url)
             page_html.raise_for_status()
@@ -50,15 +51,16 @@ for i in range(1, 50):  # chose max page num to be 50 randomly
             continue    # if the above code ran, the execution won't reach here
         else:
             break
-    
-    # Maybe page_html can't appear here, because its in a different scope. See what happens.
-    page_soup = bs4.BeautifulSoup(page_html.text, 'lxml')
+
+    # Get epaper page's pic url
+    page_soup = bs4.BeautifulSoup(page_html.text)
     pic_elem = page_soup.select_one('#main-image-page')
     if pic_elem is None:
         print('Last page has been downloaded.')
         sys.exit()
     pic_src = pic_elem.get('src')
 
+    # Download the pic url
     try_counter = 0
     while try_counter < 6:
         try:
